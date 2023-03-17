0a86d1a320640f08996860188b1eb876f554fae5
Rhodoni <alex.amsif@master-developpement-logiciel.fr>
2023-03-17 09:16:27 +0100

    core code

--- app/src/main/java/com/ut3/lethedudragon/game/GameThread.java
+++ app/src/main/java/com/ut3/lethedudragon/game/GameThread.java
@@ -1,4 +1,53 @@
 package com.ut3.lethedudragon.game;
 
-public class GameThread {
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public class GameThread extends Thread {
+
+    private final SurfaceHolder surfaceHolder;
+    private GameView gameView;
+    private Canvas canvas;
+    private boolean running;
+
+    private long lastTime;
+    private final long framerate = 30;
+
+    public GameThread(SurfaceHolder surfaceHolder, GameView gameView) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gameView = gameView;
+    }
+
+    private void setRunning(boolean isRunning) {this.running = isRunning;}
+
+    @Override
+    public void run(){
+        while(running){
+            long currentTime = System.currentTimeMillis();
+
+            if (currentTime - lastTime > 1000/framerate) {
+                lastTime = currentTime;
+
+                canvas = null;
+                try {
+                    canvas = this.surfaceHolder.lockCanvas();
+                    synchronized (surfaceHolder) {
+                        this.gameView.draw(canvas);
+                        this.gameView.update();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (canvas != null) {
+                        try {
+                            surfaceHolder.unlockCanvasAndPost(canvas);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
